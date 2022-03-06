ccf652853247a222ad798f02f4188a39d703812f
krishnapriya j <97333237+J-krishna-123@users.noreply.github.com>
2022-03-06 08:55:56 +0530

    Delete sum of odd even.py

--- sum of odd even.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter a number"))
-eventotal=0
-oddtotal=0
-for i in range(1,n+1):
-    if(i%2==0):
-        eventotal=eventotal+i
-    else:
-        oddtotal=oddtotal+i
-print("even totel=",eventotal,"odd totel=",oddtotal)
